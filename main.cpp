b002235a36a82ed300c91e782aef909096450e37
Malte Eberhardt <malte-eberhardt@gmx.de>
2026-04-17 09:14:14 +0000

    added 1 satz

--- main.cpp
+++ main.cpp
@@ -21,6 +21,7 @@ int main()
               << std::endl
               << std::endl; // erster Satz, neue Sätze in diesem Kapitel erst
                             // unter diesem Satz
+   std::cout << "Daher gingen sie zur Agentur für Arbeit." << std::endl;
 
     /*
      * Zweites Kapitel
