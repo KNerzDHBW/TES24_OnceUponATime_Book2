9d6089e086de326589d3d3012b2b821b4a7d1c5b
Fritz <fritzmariakuss@gmail.com>
2026-04-17 11:13:23 +0200

    bin lyriker

--- main.cpp
+++ main.cpp
@@ -47,7 +47,9 @@ int main()
               << std::endl
               << std::endl; // erster Satz, neue Sätze in diesem Kapitel erst
                             // unter diesem Satz
-
+    std::cout << "Es war immer tiefste Nacht, bitter kalt und selbst der Mond schien langsam seine letzte Kraft zu verlieren."
+               << std::endl
+               << std::endl;
     /*
      * Viertes Kapitel
      */
