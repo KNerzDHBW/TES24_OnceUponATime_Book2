52b313fe2266af575d3afdb1ffceba9f4d4542c1
Levi Grenz <levigrenz@web.de>
2026-04-17 11:25:47 +0200

    machmalfritz

--- main.cpp
+++ main.cpp
@@ -90,5 +90,5 @@ int main()
         << std::endl // erster Satz, neue Sätze in diesem Kapitel erst
                       // unter diesem Satz
          << "In diesem Schrottplatz gab es eine kleine Schaukel, die von einem alten Mann gebaut wurde, der einst ein berühmter Erfinder war.";
-    std::cout << "Der Vogel bekam Hunger und hat die anderen gegessen." << std::endl;                 
+    std::cout << "Der Schrottplatz war aber so alt, es gab eine kleine Besonderheit." << std::endl;                 
 }
